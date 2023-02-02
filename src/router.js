409f4cf6e04a3af4ea226feb034c3254cba532f7
Amjad King <amjadbarchini40@gmail.com>
2023-02-02 17:15:59 +0200

    my-Find-Coach-app

---
+++ src/router.js
@@ -0,0 +1,42 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import CoachDetail from './pages/coaches/CoachDetail.vue'
+import CoachesList from './pages/coaches/CoachesList.vue'
+import CoachRegistration from './pages/coaches/CoachRegistration.vue'
+import ContactCoach from './pages/requests/ContactCoach.vue'
+import RequestRecieved from './pages/requests/RequestsReceived.vue'
+import NotFound from './pages/NotFound.vue'
+import UserAuth from './pages/auth/UserAuth.vue'
+import store from "./store/index.js";
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', redirect: '/coaches'},
+        { path: '/coaches', component: CoachesList },
+        { 
+            path: '/coaches/:id',
+            component: CoachDetail,
+            props: true,
+            children: [
+                { path: 'contact', component: ContactCoach },
+            ]
+        },
+        { path: '/register', component: CoachRegistration, meta: { requiresAuth: true}},
+        { path: '/requests', component: RequestRecieved, meta: { requiresAuth: true} },
+        { path: '/auth', component: UserAuth, meta: { requiresAuth: true}},
+        { path: '/:notFound(.*)', component: NotFound },
+    ], 
+})
+
+router.beforeEach(function(to, _, next) {
+    if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+        next('/auth');
+    } else if (to.meta.requiresAuth && store.getters.isAuthenticated) {
+        next('/coaches')
+    } else {
+        next()
+    }
+})
+
+export default router;
